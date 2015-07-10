5f17c9701990f18f8400ea2754cb47f7c430a94d
Blake Watters <blakewatters@gmail.com>
2015-07-09 18:00:22 -0700

    Convert from UUID to URL syntax

--- QuickStart/LQSAppDelegate.m
+++ QuickStart/LQSAppDelegate.m
@@ -196,7 +196,7 @@ static NSString *const LQSLayerAppIDString = @"LAYER_APP_ID";
         /*
          * 2. Acquire identity Token from Layer Identity Service
          */
-        [self requestIdentityTokenForUserID:userID appID:[self.layerClient.appID UUIDString] nonce:nonce completion:^(NSString *identityToken, NSError *error) {
+        [self requestIdentityTokenForUserID:userID appID:[self.layerClient.appID absoluteString] nonce:nonce completion:^(NSString *identityToken, NSError *error) {
             if (!identityToken) {
                 if (completion) {
                     completion(NO, error);
